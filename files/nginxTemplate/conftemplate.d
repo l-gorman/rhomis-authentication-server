7fd8579d2e275e42b6cd0f43de7f0bfc8ece6d76
l-gorman <lgorman@turing.ac.uk>
2021-11-15 09:39:18 +0000

    modifying template

--- files/nginxTemplate/conftemplate.d
+++ files/nginxTemplate/conftemplate.d
@@ -2,9 +2,11 @@ server {
     listen 80;
     server_name example.org;
 
+    server_tokens off;
+
     location /.well-known/acme-challenge/ {
     root /var/www/certbot;
-}
+    }
 
     location / {
         return 301 https://$host$request_uri;
@@ -14,6 +16,12 @@ server {
         listen 80;
 	listen 443 ssl;
         server_name example.org;
+        server_tokens off;
+
+        ssl_certificate /etc/letsencrypt/live/example.org/fullchain.pem;
+        ssl_certificate_key /etc/letsencrypt/live/example.org/privkey.pem;
+        include /etc/letsencrypt/options-ssl-nginx.conf;
+        ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
 
         location / {
                 proxy_pass http://localhost:3002;
@@ -24,6 +32,5 @@ server {
                 proxy_cache_bypass $http_upgrade;
         }
 
-        ssl_certificate /etc/letsencrypt/live/example.org/fullchain.pem;
-        ssl_certificate_key /etc/letsencrypt/live/example.org/privkey.pem;
+        
 }
