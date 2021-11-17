6c1268a5a5628ac675fca628eb01d77ca93b8c7a
l-gorman <lgorman@turing.ac.uk>
2021-11-17 13:17:42 +0000

    adding theability to start the server container in the init script

--- templates/init-letsencrypt-template.sh
+++ templates/init-letsencrypt-template.sh
@@ -13,6 +13,9 @@ data_path="./data/certbot"
 # email="" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
+echo "Starting the Node Server and Database"
+docker-compose up -d rhomis-auth-server
+
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
@@ -80,3 +83,7 @@ echo
 
 echo "### Reloading nginx ..."
 docker-compose exec nginx nginx -s reload
+
+
+echo "Stopping the Node Server and Database"
+docker-compose down
